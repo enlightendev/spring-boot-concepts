60e71e3973023003f3e94058146afa961daefdb5
Juan <enlightendevio@gmail.com>
2016-07-14 22:14:13 -0400

    basic controller to test various configuration of request parameters, request/response headers, etc along with basic log4j2 configuration.

---
+++ src/main/java/io/enlightendev/controllers/MainController.java
@@ -0,0 +1,76 @@
+package io.enlightendev.controllers;
+
+import io.enlightendev.domain.Greeting;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * This code uses Spring 4’s new @RestController annotation, which marks the
+ * class as a controller where every method returns a domain object instead
+ * of a view. It’s shorthand for @Controller and @ResponseBody rolled together.
+ */
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+    private final Logger log = LogManager.getLogger(this.getClass());
+
+    private static final String template = "Hello, %s!";
+
+    private final AtomicLong counter = new AtomicLong();
+
+    /**
+     * Request params are appended to url
+     * @param name
+     * @return
+     */
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="world") String name) {
+
+        log.debug("greeting( name: {} )" + name);
+
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
+    }
+
+    /**
+     *
+     * @param userAgent
+     * @param clientId
+     * @return
+     */
+    @RequestMapping("/requestHeaders")
+    public @ResponseBody ResponseEntity<Greeting> requestHeaders (
+            @RequestHeader(value="user-agent", defaultValue="foo") String userAgent,
+            @RequestHeader(value="client-id", defaultValue="1234") String clientId ){
+
+        Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, "world"));
+
+        return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    @RequestMapping(value = "responseHeaders", method = RequestMethod.GET)
+    public @ResponseBody ResponseEntity<Greeting> responseHeaders(){
+
+        Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, "world"));
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("RequestID", "aaa-123");
+        responseHeaders.set("ResponseID", "res-8765");
+
+        return new ResponseEntity<Greeting>(greeting, responseHeaders, HttpStatus.OK);
+
+    }
+
+}
